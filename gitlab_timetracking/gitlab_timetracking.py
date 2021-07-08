460ab8d9d12923868ae623fbd61501c9c36575ed
Chris <ulrich@tcs-germany.de>
2021-07-08 14:26:34 +0200

    Hello World

---
+++ gitlab_timetracking/gitlab_timetracking.py
@@ -0,0 +1,7 @@
+import timesheet_gitlab,gitlab
+
+class GitLabTimeTracking():
+    def run(self):
+        print("Hello World !")
+if __name__ == "__main__":
+    GitLabTimeTracking().run()
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='gitlab_timetracking',
+      version='0.0.1',
+      description='simple timetracking commandline application that uses the Gitlab api to get / store times in gitlab directly in the project/repository',
+      url='https://github.com/cutec-chris/gitlab-timetracking',
+      author='Christian Ulrich',
+      author_email='github@chris.ullihome.de',
+      license='MIT',
+      python_requires='>=3.6',
+      packages=['gitlab_timetracking'],
+      scripts=['bin/tt'],
+      zip_safe=False,
+      include_package_data=True,
+      install_requires=['python-gitlab','timesheet-gitlab']
+      )
